accfee708887d9ef69dd3964c0647910f41d27b9
Daniel Schutte <daniel@omfs.co.za>
2025-05-18 14:03:51 +0200

    Refactor decode_field and encode_field functions to support both string and integer field identifiers

--- lib/iso_8583/client/ppn.ex
+++ lib/iso_8583/client/ppn.ex
@@ -6,27 +6,36 @@ defmodule ISO8583.Client.PPN do
   @behaviour ISO8583.Client.Base
 
   @impl true
-  def decode_field("120", data) do
+  def decode_field("120", data), do: decode_field(120, data)
+  def decode_field(120, data) do
     ISO8583.Client.PPN.DE120.unpack_de120(data)
   end
 
-  def decode_field(field, _data) do
+  def decode_field(field, _data) when is_binary(field) do
+    {:error, "Field #{field} not implemented for PPN client"}
+  end
+  def decode_field(field, _data) when is_integer(field) do
     {:error, "Field #{field} not implemented for PPN client"}
   end
 
   @impl true
-  def encode_field("120", sub_fields) do
+  def encode_field("120", sub_fields), do: encode_field(120, sub_fields)
+  def encode_field(120, sub_fields) do
     ISO8583.Client.PPN.DE120.pack_de120(sub_fields)
   end
 
-  def encode_field(field, _sub_fields) do
+  def encode_field(field, _sub_fields) when is_binary(field) do
+    {:error, "Field #{field} not implemented for PPN client"}
+  end
+  def encode_field(field, _sub_fields) when is_integer(field) do
     {:error, "Field #{field} not implemented for PPN client"}
   end
 
   @doc """
   Returns the list of sub-fields for a given field.
   """
-  def get_sub_fields("120") do
+  def get_sub_fields("120"), do: get_sub_fields(120)
+  def get_sub_fields(120) do
     [
       "120.1", "120.45", "120.46", "120.47", "120.50", 
       "120.56", "120.62", "120.70", "120.71", "120.72", 
